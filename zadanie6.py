be93f01f776931a9e18750fc591a3b9ad77253b8
alisashch <alisa.scherbinina@gmail.com>
2024-11-07 12:23:27 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,3 @@
+# W programowaniu, pętla oznacza powtarzanie czegoś wielokrotnie. 
+# w pętli while, coś się powtarza dopóki warunek jest True, a pętla for
+# powtarza coś określoną ilość razy lub dla określonej rzeczy  
