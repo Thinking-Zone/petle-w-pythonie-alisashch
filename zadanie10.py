d38e4137751806216f9fb0d5a9a2b27c4c22be8d
alisashch <alisa.scherbinina@gmail.com>
2024-11-21 11:40:49 +0100

    Update zadanie10.py

---
+++ zadanie10.py
@@ -0,0 +1,9 @@
+#wypisz te imiona pod sobą: apolonia, alisa, marika, lena, sofiia
+
+
+imie_kogos = ['apolonia', 'alisa', 'marika', 'lena', 'sofiia']
+
+print(imie_kogos[0])
+print(imie_kogos[1])
+print(imie_kogos[2])
+print(imie_kogos[3])
